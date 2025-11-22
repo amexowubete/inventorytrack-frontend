a95e153a5cdd97e847867fbcefcfce82bc71914a
amexowubete <125564654+amexowubete@users.noreply.github.com>
2025-11-22 23:29:52 +0300

    Remove root configuration from Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './', // tell Vite to use the root folder
   plugins: [react()]
 })
