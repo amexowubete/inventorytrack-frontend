c8b103a8a61f349a2fd9b2035e6dce3e3f1570a4
amexowubete <125564654+amexowubete@users.noreply.github.com>
2025-11-22 23:24:44 +0300

    Configure Vite root directory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  root: './', // tell Vite to use the root folder
+  plugins: [react()]
+})
