548eff33c94241d9511fa5ec2f8e78862d3ce070
amexowubete <125564654+amexowubete@users.noreply.github.com>
2025-11-23 09:10:02 +0300

    Fix formatting and resolve merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-<<<<<<< HEAD
-export default defineConfig({ plugins: [react()] })
-=======
 export default defineConfig({
-  plugins: [react()]
-})
->>>>>>> f6ae393435a5c606a90ba93bef9b4a3e4b2ecd2c
+  plugins: [react()],
+});
